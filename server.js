62d6e07f4b309ce9ce5d2d961c861b371165d4c8
jameelfullstackdev <jameel@jameelahmed.com.pk>
2020-05-19 21:24:13 +0500

    connected to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("it is running wow"));
+app.get("/", (req, res) => res.send("it is running on heroku wow"));
 
 const PORT = process.env.PORT || 5000;
 
